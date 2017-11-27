40d293b8cb3d8b2fe2f978aab6c742255db3ff25
Hannah Bourke <hannah25@gmail.com>
2017-11-27 12:13:34 +1100

    Add emojis

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
+import Emoji from './components/Emoji';
 
 class App extends Component {
   render() {
@@ -12,9 +13,12 @@ class App extends Component {
         <Button title="Sign up" />
         <Button title='Enter competition' magic />
         <ul>
-          <li>First</li>
-          <li>Second</li>
-          <li>Third</li>
+          <li><Emoji /></li>
+          <li><Emoji emotion = 'sad' /></li>
+          <li><Emoji emotion = 'love' /></li>
+          <li><Emoji cat /></li>
+          <li><Emoji cat emotion = 'sad' /></li>
+          <li><Emoji cat emotion = 'love' /></li>
           </ul>
         <p>No more content</p>
       </div>
---
+++ src/components/Emoji.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Emoji({
+  emotion = 'happy',
+  cat = false
+}) {
+  let text = ''
+  if (cat) {
+    if (emotion === 'sad') {
+        text = '😿'
+    } else if (emotion === 'love'){
+        text = '😻'
+   } else {
+      text = '😺'
+    }
+  } else {
+    if (emotion === 'sad') {
+        text = '😩'
+    } else if (emotion === 'love') {
+        text = '😍'
+    } else {
+      text = '😊'
+    }
+  }
+
+  return (
+    <span>
+      { text }
+    </span>
+  )
+}
+
+export default Emoji
+// module.exports = Emoji
