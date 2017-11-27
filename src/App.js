68148c64f325b0675c6f84ec17eb78e564577d4f
Hannah Bourke <hannah25@gmail.com>
2017-11-27 11:20:50 +1100

    Add button component

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>This is a heading</h1>
+        <Button title='Find Flights' primary={true} />
+        <Button title="Sign up" />
+        <Button title='Enter competition' magic={true} />
+        <ul>
+          <li>First</li>
+          <li>Second</li>
+          <li>Third</li>
+          </ul>
+        <p>No more content</p>
+      </div>
+    );
+  }
+}
+
+export default App;
